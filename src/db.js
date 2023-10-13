b865cc75f19bed7cf0731db5ecad02d8fda57f8d
Drake4044 <martincastro4044@gmail.com>
2023-10-12 21:25:03 -0300

    deployd

--- src/db.js
+++ src/db.js
@@ -1,4 +1,4 @@
-// require('dotenv').config();
+require('dotenv').config();
 import dot from "dotenv"
 dot.config()
 // const { Sequelize } = require("sequelize");
@@ -9,9 +9,10 @@ import TodoModel from "./models/Todo.js"
 import UserModel from "./models/User.js"
 
 const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME } = process.env;
+const urlDeployd = "postgres://database_todo_app_user:CFGXOOcNBmj2uJurvSyA940Qp5eJPf0N@dpg-ckk8qpbj89us739sbvj0-a.oregon-postgres.render.com/database_todo_app"
 
 
-export const dataBase = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@localhost/${DB_NAME}`, {
+export const dataBase = new Sequelize(`${urlDeployd}`, {
   logging: false,
   native: false,
 })
